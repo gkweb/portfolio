126dc80f14d0444821bb5246c8e743cfd2b30560
gkweb <gladekettle@gmail.com>
2017-02-03 12:01:48 +1100

    Cleaned up package.js for packaging on server

--- package.js
+++ package.js
@@ -1,11 +1,9 @@
 const fs = require('fs')
 const pug = require('pug')
-const requireDir = require('require-dir')
-
-const content = requireDir('./content', {recurse: true})
+const content = require('./data/content')
 
 // render and write file
-fs.writeFile('./public/index.html', pug.renderFile('./public/components/app/index.pug', content.home), error => {
+fs.writeFile('./index.html', pug.renderFile('./components/app/index.pug', content), error => {
   if (error) console.log('Error rendering HTML', error)
   else console.log('Rendered HTML')
 })
